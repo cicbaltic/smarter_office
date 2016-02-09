0795216234692b811c0dccb76b33b4d67ff012ac
Norbertas Mazeika <nmazeika@lt.ibm.com>
2016-02-09 10:18:09 +0200

    first commit

---
+++ core/app.js
@@ -0,0 +1,40 @@
+/*eslint-env node*/
+
+//------------------------------------------------------------------------------
+// node.js starter application for Bluemix
+//------------------------------------------------------------------------------
+
+// This application uses express as its web server
+// for more info, see: http://expressjs.com
+var express = require('express');
+
+// cfenv provides access to your Cloud Foundry environment
+// for more info, see: https://www.npmjs.com/package/cfenv
+var cfenv = require('cfenv');
+
+// create a new express server
+var app = express();
+var router = express.Router();
+
+// ROUTES FOR REST API
+router.get('/', function (req, res) {
+    res.json({
+        message: 'hooray!welcome to our api!'
+    });
+});
+
+// serve the files out of ./public as our main files
+app.use(express.static(__dirname + '/public'));
+
+// register api routes
+app.use('/api', router);
+
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+// start server on the specified port and binding host
+app.listen(appEnv.port, '0.0.0.0', function () {
+
+    // print a message when the server starts listening
+    console.log("server starting on " + appEnv.url);
+});
