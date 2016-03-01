005098e470a5b8ed7bcce39de5fc0c7f2247b0d2
Giedrius Valintelis <giedrius.valintelis@lt.ibm.com>
2016-03-01 20:11:45 +0100

    Cloundant DB URL configurated to be taken from VCAP_SERVICES. For local environment VCAP_SERVICES.json to be used (added in .gitignore list - must not be commited).

--- core/config.js
+++ core/config.js
@@ -1,3 +1,14 @@
-exports.cloudant_url = <URL removed, contains credentials>;
-
-
+var vcapServices;
+if (process.env.VCAP_SERVICES) {
+    vcapServices = JSON.parse(process.env.VCAP_SERVICES);
+} else {
+    try {
+        // for local environment only
+        // file has to be placed in ./core directory
+        // and should not be commited
+        vcapServices = require('./VCAP_SERVICES.json');
+    } catch (e) {
+        console.error(e);
+    }
+}
+exports.dbUrl = vcapServices.cloudantNoSQLDB[0].credentials.url;
