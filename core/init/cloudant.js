7fd36ec40c846fb20edde4fe3b276ba6ac44f2e3
Aurimas Bartninkas <abartnin@lt.ibm.com>
2016-03-03 11:54:42 +0200

    Make cloudant.js reusable

--- core/init/cloudant.js
+++ core/init/cloudant.js
@@ -2,33 +2,47 @@ var config = require('./../config');
 var nano = require("nano")(config.dbUrl);
 var smartOfficeEnvDB = nano.use('office_environment');
 
-var _design = '_design/';
+var temperatureWithHumName = 'temperatureWithHum';
+var temperatureWithHumObj= require("./designDocuments/temperatureWithHum.js");
 
-var designDocumentName = _design + 'temperatureWithHum';
 
-var temperatureWithHum = require("./designDocuments/temperatureWithHum.js");
+var temperatureWithHum = new CloudantDesignDoc(temperatureWithHumName, temperatureWithHumObj);
+temperatureWithHum.createDesignObject();
 
-smartOfficeEnvDB.get(designDocumentName, { revs_info: true }, function(err, body) {
-	if (!err) {
-		console.log('Design document: ' + designDocumentName + ' was found.');
-		smartOfficeEnvDB.destroy(designDocumentName, body._rev, function(err, body) {
+
+
+function CloudantDesignDoc(designDocName, obj) {
+	var _design = '_design/';
+	this.docName = _design + designDocName;
+	this.obj = obj;
+
+	this.createDesignObject = function() {
+		console.log(this.docName);
+		var desgnDocName = this.docName;
+		var desgnDocObj = this.obj;
+		smartOfficeEnvDB.get(desgnDocName, { revs_info: true }, function(err, body) {
 			if (!err) {
-				console.log('Design document: ' + designDocumentName + ' was removed from database.');
-				insertDesignDocument(temperatureWithHum, designDocumentName);
+				console.log('Design document: ' + desgnDocName + ' was found.');
+				smartOfficeEnvDB.destroy(desgnDocName, body._rev, function(err, body) {
+					if (!err) {
+						console.log('Design document: ' + desgnDocName + ' was removed from database.');
+						insertDesignDocument(desgnDocName, desgnDocObj);
+					}
+				});
+			} else {
+				console.log('Design document: ' + desgnDocName + ' not found.');
+				insertDesignDocument(desgnDocName, desgnDocObj);
 			}
 		});
-	} else {
-		console.log('Design document: ' + designDocumentName + ' not found.');
-		insertDesignDocument(temperatureWithHum, designDocumentName);
 	}
-});
 
-function insertDesignDocument(temprWithHum, docName) {
-	smartOfficeEnvDB.insert(temprWithHum, docName, function(err, response){
-		if(!err) {
-			console.log('New design document: ' + docName + ' was created.');
-		} else {
-			console.log(err);
-		}
-	});
+	insertDesignDocument = function(docName, obj) {
+		smartOfficeEnvDB.insert(obj, docName, function(err, response){
+			if(!err) {
+				console.log('New design document: ' + docName + ' was created.');
+			} else {
+				console.log(err);
+			}
+		});
+	}
 }
