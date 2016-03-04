89306d33f20a6850a007814644d46ef70553e615
marius <marius@Mac>
2016-03-04 08:46:47 +0200

    Added some basic layout with charts and support for websockets

---
+++ native/README.md
@@ -0,0 +1,15 @@
+**IBM CIC Baltic Smarter Office**
+
+For lib management we started using [cocoapods](https://cocoapods.org/)
+
+    $ sudo gem install cocoapods
+
+To install libs run these inside "native" folder:
+
+    $ pod install
+
+    $ open smarter-office.xcworkspace
+
+We are currently using
+[Charts](https://github.com/danielgindi/ios-charts) 2.2.3
+[Starscream](https://github.com/daltoniam/Starscream) 1.1.1 - websocket support
--- native/smarter-office/FirstViewController.swift
+++ native/smarter-office/FirstViewController.swift
@@ -7,19 +7,102 @@
 //
 
 import UIKit
+import Charts
+import Starscream
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, WebSocketDelegate {
+    @IBOutlet weak var temp: UILabel!
+    @IBOutlet var lineChartView: LineChartView!
+    
+    var socket = WebSocket(url: NSURL(string: "ws://localhost:8001/")!)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        let unitsSold = [18.0, 18.5, 18.0, 19.0, 18.5, 20.0, 19.5, 18.5, 19.0, 18.0, 18.5, 18.0]
+
+        //self.setChart(months, values: unitsSold)
+        self.setChartData(months, unitsSold: unitsSold)
+        socket.delegate = self
+        socket.connect()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func setChart(dataPoints: [String], values: [Double]) {
+        lineChartView.noDataText = "No data for the chart."
+        
+        var dataEntries: [BarChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count {
+            let dataEntry = BarChartDataEntry(value: values[i], xIndex: i)
+            dataEntries.append(dataEntry)
+        }
+        
+        let chartDataSet = BarChartDataSet(yVals: dataEntries, label: "Units Sold")
+        let chartData = BarChartData(xVals: dataPoints, dataSet: chartDataSet)
+        lineChartView.data = chartData
+    }
+    
+    func setChartData(months : [String], unitsSold: [Double]) {
+        // 1 - creating an array of data entries
+        var yVals1 : [ChartDataEntry] = [ChartDataEntry]()
+        for var i = 0; i < months.count; i++ {
+            yVals1.append(ChartDataEntry(value: unitsSold[i], xIndex: i))
+        }
+        
+        // 2 - create a data set with our array
+        let set1: LineChartDataSet = LineChartDataSet(yVals: yVals1, label: "First Set")
+        set1.axisDependency = .Left // Line will correlate with left axis values
+        set1.setColor(UIColor.redColor().colorWithAlphaComponent(0.2)) // our line's opacity is 50%
+        set1.setCircleColor(UIColor.redColor()) // our circle will be dark red
+        set1.lineWidth = 2.0
+        set1.circleRadius = 6.0 // the radius of the node circle
+        set1.fillAlpha = 65 / 255.0
+        set1.fillColor = UIColor.redColor()
+        set1.highlightColor = UIColor.whiteColor()
+        set1.drawCircleHoleEnabled = true
+        
+        //3 - create an array to store our LineChartDataSets
+        var dataSets : [LineChartDataSet] = [LineChartDataSet]()
+        dataSets.append(set1)
+        
+        //4 - pass our months in for our x-axis label value along with our dataSets
+        let data: LineChartData = LineChartData(xVals: months, dataSets: dataSets)
+        data.setValueTextColor(UIColor.blackColor())
+        
+        //5 - finally set our data
+        self.lineChartView.backgroundColor = UIColor.whiteColor()
+        self.lineChartView.getAxis(.Left).drawGridLinesEnabled = false
+        self.lineChartView.xAxis.drawGridLinesEnabled = false
+        self.lineChartView.data = data
+        self.lineChartView.xAxis.labelPosition = .Bottom
+    
+    }
 
-
+    func websocketDidConnect(socket: WebSocket) {
+        print("websocket is connected")
+    }
+    
+    func websocketDidDisconnect(socket: WebSocket, error: NSError?) {
+        print("websocket is disconnected: \(error?.localizedDescription)")
+    }
+    
+    func websocketDidReceiveMessage(socket: WebSocket, text: String) {
+        print("got some text: \(text)")
+        print(self.temp)
+        if(self.temp != nil) {
+            self.temp.text = text + "° C";
+        }
+    }
+    
+    func websocketDidReceiveData(socket: WebSocket, data: NSData) {
+        print("got some data: \(data.length)")
+    }
+    
 }
 
