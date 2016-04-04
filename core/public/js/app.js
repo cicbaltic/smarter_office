e154061a6a86ad49c48b70c347c5b6b6aa6db04d
Norbertas Mazeika <nmazeika@lt.ibm.com>
2016-04-04 08:37:52 +0300

    added refresh button

--- core/public/js/app.js
+++ core/public/js/app.js
@@ -1,34 +1,33 @@
 var app = angular.module('smarterOfficeApp', ["chart.js"]);
 
 app.controller('tempAndHumCtrl', function ($scope, $http, $timeout) {
-    $http.get("http://cicb-smarter-office.stage1.mybluemix.net/api/latestTemperaturesAndHumByZoneIds").then(function (response) {
-        $scope.myData = response.data.rows;
+    'use strict';
+    $scope.updateData = function () {
+        $http.get("http://cicb-smarter-office.stage1.mybluemix.net/api/latestTemperaturesAndHumByZoneIds").then(function (response) {
+            $scope.myData = response.data.rows;
 
-        angular.forEach($scope.myData, function (entry) {
-            var hum = entry.hum_v;
-            var temp = entry.temp_v;
-            entry.humChart = {
-                data: [
-                    hum, 100 - hum
-                ],
-                labels: ["Humidity", "Other"]
-            }
-            entry.tempChart = {
-                data: [
-                    entry.temp_v, 50 - temp
-                ],
-                labels: ["Temperature", "Other"]
-            }
-            entry.options = {
-                showTooltips: false,
-                percentageInnerCutout: 70,
-                responsive: false
-            };
-            var date = new Date(entry.date);
-            var day = moment(entry.date);
-            entry.formatedDate = moment(entry.date).format("YYYY-MM-DD HH:mm");
+            angular.forEach($scope.myData, function (entry) {
+                var hum = entry.hum_v,
+                    temp = entry.temp_v,
+                    day = moment(entry.date);
+                entry.humChart = {
+                    data: [hum, 100 - hum],
+                    labels: ["Humidity", "Other"]
+                };
+                entry.tempChart = {
+                    data: [entry.temp_v, 50 - temp],
+                    labels: ["Temperature", "Other"]
+                };
+                entry.options = {
+                    showTooltips: false,
+                    percentageInnerCutout: 70,
+                    responsive: false
+                };
+                entry.formatedDate = moment(entry.date).format("YYYY-MM-DD HH:mm");
+            });
+
         });
-
-    });
+    };
+    $scope.updateData();
 
 });
