2275759a57ab3ac1aa5b53398e5ed389ab87f723
Laurynas <lkacinau@lt.ibm.com>
2016-05-03 09:40:09 +0300

    iBeacons service added, left side menu added

--- hybrid/www/js/Services.js
+++ hybrid/www/js/Services.js
@@ -1,51 +1,77 @@
 var services = angular.module('starter.Services', []);
 
-services.factory('TemperatureAndHumidityService', [
-		'$http','ApiEndpoint',
-		function($http, ApiEndpoint) {
-			return {
-				getTemperature : function() {
-					return $http
-					.get(
-							// ApiEndpoint.url
-							// 		+ '/temperaturesAndHumWithRange/2016-03-30/2016-03-30T23:59/1',
-							 'js/mockData/temperaturesAndHumWithRangeAndZoneId.json',
-									{
-										timeout: 5000
-									}).then(
-							function successCallback(response) {
-								var messageSize = response.data.size;
-								var data = [];
-								for(i = 0; i<messageSize; i++){
-									if(data[ response.data.rows[i].zone_id ] == null){
-										data[ response.data.rows[i].zone_id ]={
-												temperature : response.data.rows[i].temp_v,
-												humidity : response.data.rows[i].hum_v,
-												zone : response.data.rows[i].zone_id,
-												timestamp : response.data.rows[i].timestamp
-											}
-									}
-									else if(data[ response.data.rows[i].zone_id ].timestamp < response.data.rows[i].timestamp){
-										data[ response.data.rows[i].zone_id ]={
-											temperature : response.data.rows[i].temp_v,
-											humidity : response.data.rows[i].hum_v,
-											zone : response.data.rows[i].zone_id,
-											timestamp : response.data.rows[i].timestamp
-										}
-									}
-								}
-								var responseData = [];
+services.factory('TemperatureAndHumidityService', ['$http', 'ApiEndpoint', function ($http, ApiEndpoint) {
+    return {
+        getTemperature: function () {
+            return $http
+                .get(
+                    // ApiEndpoint.url
+                    // 		+ '/temperaturesAndHumWithRange/2016-03-30/2016-03-30T23:59/1',
+                    'js/mockData/temperaturesAndHumWithRangeAndZoneId.json', {
+                        timeout: 5000
+                    }).then(
+                    function successCallback(response) {
+                        var messageSize = response.data.size;
+                        var data = [];
+                        for (i = 0; i < messageSize; i++) {
+                            if (data[response.data.rows[i].zone_id] == null) {
+                                data[response.data.rows[i].zone_id] = {
+                                    temperature: response.data.rows[i].temp_v,
+                                    humidity: response.data.rows[i].hum_v,
+                                    zone: response.data.rows[i].zone_id,
+                                    timestamp: response.data.rows[i].timestamp
+                                }
+                            } else if (data[response.data.rows[i].zone_id].timestamp < response.data.rows[i].timestamp) {
+                                data[response.data.rows[i].zone_id] = {
+                                    temperature: response.data.rows[i].temp_v,
+                                    humidity: response.data.rows[i].hum_v,
+                                    zone: response.data.rows[i].zone_id,
+                                    timestamp: response.data.rows[i].timestamp
+                                }
+                            }
+                        }
+                        var responseData = [];
 
-								var i = 0;
-								for (var dat in data) {
+                        var i = 0;
+                        for (var dat in data) {
 
-									responseData[i]=data[dat];
-									 i++;
-									}
-								return responseData;
-							}, function errorCallback(response) {
-								return [];
-							});
-				}
-			}
+                            responseData[i] = data[dat];
+                            i++;
+                        }
+                        return responseData;
+                    },
+                    function errorCallback(response) {
+                        return [];
+                    });
+        }
+    }
 		}]);
+
+services.factory('BeaconUsersCountService', function ($http) {
+    var usersCount = [];
+    var newUsersCount = [];
+
+    return {
+        getUsers: function () {
+            return $http.get("js/mockData/iBeaconUsersCount.json").then(function (response) {
+                usersCount = response.data;
+                var newUsersCount = usersCount.count + 1;
+                return usersCount;
+            });
+        },
+
+        updateUsers: function () {
+            return $http({
+                method: "post",
+                url: "js/mockData/iBeaconUsersCount.json",
+                data: {
+                    "count": newUsersCount
+                },
+                headers: {
+                    'Content-Type': 'application/json; charset=utf-8'
+                }
+            });
+
+        }
+    }
+})
--- hybrid/www/js/app.js
+++ hybrid/www/js/app.js
@@ -3,54 +3,86 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var myApp = angular.module('starter', [ 'ionic', 'starter.Directives',
-		'starter.Controllers', 'starter.Services', 'chart.js']);
+var myApp = angular.module('starter', ['ionic', 'starter.Directives',
+		'starter.Controllers', 'starter.Services', 'chart.js', 'ngCordovaBeacon'])
 
-myApp.run(function($ionicPlatform, $rootScope, $state) {
-	$ionicPlatform.ready(function() {
-		if (window.cordova && window.cordova.plugins.Keyboard) {
-			// Hide the accessory bar by default (remove this to show the
-			// accessory bar above the keyboard
-			// for form inputs)
-			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+.run(function ($ionicPlatform, $rootScope, $state) {
+    $ionicPlatform.ready(function () {
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            // Hide the accessory bar by default (remove this to show the
+            // accessory bar above the keyboard
+            // for form inputs)
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-			// Don't remove this line unless you know what you are doing. It
-			// stops the viewport
-			// from snapping when text inputs are focused. Ionic handles this
-			// internally for
-			// a much nicer keyboard experience.
-			cordova.plugins.Keyboard.disableScroll(true);
-		}
-		if (window.StatusBar) {
-			StatusBar.styleDefault();
-		}
-	});
-	$rootScope.$on('$stateChangeSuccess', function(event, next, current) {
-		if ($state.current.name === 'index'){
-			$rootScope.$emit('indexRefresh');
-		}
-	});
-});
+            // Don't remove this line unless you know what you are doing. It
+            // stops the viewport
+            // from snapping when text inputs are focused. Ionic handles this
+            // internally for
+            // a much nicer keyboard experience.
+            cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
 
-myApp.constant('Constants', {
-	temperatureConst : 30,
-	humidityConst : 100
-});
+    $rootScope.$on('$stateChangeSuccess', function (event, next, current) {
+        if ($state.current.name === 'index') {
+            $rootScope.$emit('indexRefresh');
+        }
+    });
+})
 
-myApp.constant('ApiEndpoint', {
-	url: 'http://localhost:8100/api'
-}); 
+.constant('Constants', {
+    temperatureConst: 30,
+    humidityConst: 100
+})
 
-myApp.config(function($stateProvider, $urlRouterProvider) {
-	$stateProvider.state('history', {
-		url : '/history/:zoneId/:order',
-		templateUrl : 'templates/history.html',
-		controller : 'historyController'
-	});
-	$stateProvider.state('index', {
-		url : '/',
-		templateUrl : 'templates/main.html',
-		controller : 'indexController'
-	});
-	$urlRouterProvider.otherwise('/');
+.constant('ApiEndpoint', {
+    url: 'http://localhost:8100/api'
+})
+
+.config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+    $ionicConfigProvider.navBar.alignTitle('center')
+
+    $stateProvider
+
+        .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'AppCtrl'
+    })
+
+    .state('app.home', {
+        url: '/home',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/main.html',
+                controller: 'indexController'
+            }
+        }
+    })
+
+    .state('app.ibeacon', {
+        url: '/ibeacon',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/ibeacon.html',
+                controller: 'BeaconController'
+            }
+        }
+    })
+
+    .state('app.history', {
+        url: '/history/:zoneId/:order',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/history.html',
+                controller: 'historyController'
+            }
+        }
+    });
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/home');
 });
